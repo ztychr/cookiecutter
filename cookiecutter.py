4054504a40248098e7f8c4f6da82ac72b6ca97dc
ztychr <nordskov@protonmail.com>
2019-08-20 23:17:09 +0200

    codecleaning

--- cookiecutter.py
+++ cookiecutter.py
@@ -6,6 +6,11 @@ import argparse
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+def redText(string):
+    return str(('\033[91m' + string + '\033[0m'))
+
+def boldText(string):
+    print('\033[1m' + str(string) + '\033[0m')
 
 def Main():
     # initialize the parser
@@ -15,7 +20,6 @@ def Main():
     args = parser.parse_args()
 
     cookie_list = []
-    verbosity_list = []
 
     #  get cookies from positional argument url
     try:
@@ -30,31 +34,23 @@ def Main():
     cookies = c_request.cookies
 
     if args.verbose:
-        print('\033[1m' + '\n' + '- Running program in verbose mode' + '\033[0m')
-        print('\033[1m' + '\n' + '- Fetching cookies and looking up discriptions...\n' + '\033[0m')
+        boldText('\n- Running program in verbose mode')
+        boldText('\n- Fetching cookies and looking up discriptions...\n')
 
         for cookie in tqdm(cookies):
 
             w_request = requests.get('https://cookiepedia.co.uk/cookies/' + cookie.name)
             bs = BeautifulSoup(w_request.content, 'html.parser')
 
-            name = '\033[91m' + 'name: ' + '\033[0m'  + cookie.name
-            domain = '\033[91m' + 'domain: ' + '\033[0m'  + str(cookie.domain)
-            path = '\033[91m' + 'path: ' + '\033[0m'  + str(cookie.path)
-            expire = '\033[91m' + 'expires: ' + '\033[0m'  + str(cookie.expires)
-            value = '\033[91m' + 'value: ' + '\033[0m'  + str(cookie.value)
-            version = '\033[91m' + 'version: ' + '\033[0m'  + str(cookie.version)
-            cookpedia = '\033[91m' + '\nCookpedia information: ' + '\033[0m'
+            cookie_list.append(redText('name: ') + str(cookie.name))
+            cookie_list.append(redText('domain:') + str(cookie.domain))
+            cookie_list.append(redText('path: ')  + str(cookie.path))
+            cookie_list.append(redText('expires: ') + str(cookie.expires))
+            cookie_list.append(redText('value: ') + str(cookie.value))
+            cookie_list.append(redText('version: ') + str(cookie.version))
+            cookie_list.append(redText('Cookpedia information: '))
 
             paragraphs = bs.findAll('p', limit = 6)
-
-            cookie_list.append(name)
-            cookie_list.append(domain)
-            cookie_list.append(path)
-            cookie_list.append(expire)
-            cookie_list.append(value)
-            cookie_list.append(version)
-            cookie_list.append(cookpedia)
 
             for p in paragraphs:
                 cookie_list.append(p.getText())
@@ -71,10 +67,9 @@ def Main():
             w_request = requests.get('https://cookiepedia.co.uk/cookies/' + cookie.name)
             bs = BeautifulSoup(w_request.content, 'html.parser')
 
-            name = '\033[91m' + 'Name: ' + '\033[0m'  + cookie.name + '\033[0m'
-            info = '\033[91m' + 'cookiepedia information: ' + '\033[0m'  + str(bs.find('p').text)
+            cookie_list.append(redText('name: ') + str(cookie.name))
+            info = redText('Cookiepedia information: ') + str(bs.find('p').text)
 
-            cookie_list.append(name)
             cookie_list.append(info + '\n')
 
         print('\n')
